83d798d18d9081efacf55ec9d8286fa7730418c5
BlueOCN <robgonzalezmu@gmail.com>
2024-07-11 18:50:54 -0600

    NoNotesPage Update

--- NoteApp/src/App.jsx
+++ NoteApp/src/App.jsx
@@ -1,34 +1,24 @@
+// import Split from 'react-split'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [notes, setNotes] = useState([])
 
   return (
-    <>
+    <main>
+      {notes.length <= 0 
+      ?
+      <div className='main landing'>
+        <h1 className='landing--title'>You have no notes yet</h1>
+        <button className='landing--button'>Create Note</button>
+      </div>
+      :
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Saved Notes</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      }
+    </main>
   )
 }
 
